6bce9b2e7376ef2b75fe1d4ce7099fb35296c78e
Natanael-Heinrick <natanaelheinrick12@gmail.com>
2024-11-05 21:22:13 -0300

    .add

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,19 @@ public class Main {
         System.out.println("      CADASTRO CONTATOS     ");
         System.out.println("============================");
 
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
+        // COLETANDO O NOME DOS CONTATOS
         System.out.println("Informe o primeiro contato para cadastrar: ");
         String primeiroContato = sc.nextLine();
 
         System.out.println("Informe o segundo contato para cadastrar: ");
         String segundoContato = sc.nextLine();
 
-        System.out.println("Informe o primeiro contato para cadastrar: ");
+        System.out.println("Informe o terceiro contato para cadastrar: ");
         String terceiroContato = sc.nextLine();
 
-        valoresContatos(primeiroContato, segundoContato, terceiroContato);
+        gerenciadorDeContatos(primeiroContato, segundoContato, terceiroContato, sc);
 
         sc.close();
     }
@@ -36,52 +37,60 @@ public class Main {
     }
 
     //
-    public static void valoresContatos(String primeiro, String segundo, String terceiro){
+    public static void gerenciadorDeContatos(String primeiro, String segundo, String terceiro, Scanner novoSc){
 
         // EXIBINDO A LISTA DE CONTATOS
         listaDeContatos(primeiro,segundo, terceiro);
 
-        Scanner novoSc = new Scanner(System.in);
+        // VERIFIICANDO SE O USUÁRIO QUER MODIFICAR A LISTA
+        System.out.println("Deseja apagar algum contato da lista [S/N]: ");
+        String continuar = novoSc.nextLine();
 
-        System.out.println("Qual contato você deseja retirar[1º,2º,3º]: ");
-        int numero = novoSc.nextInt();
+        if (continuar.equalsIgnoreCase("S")) {
+            System.out.println("Qual contato você deseja retirar[1º,2º,3º]: ");
+            int numero = novoSc.nextInt();
 
-        switch (numero){
-            case 1:
-                primeiro = "";
-                System.out.println("Contato 1 foi apagado");
+            // ESTRUTURA PARA APAGAR O VALOR DA LISTA
+            switch (numero) {
+                case 1:
+                    primeiro = "";
+                    System.out.println("Contato 1 foi apagado");
 
+                    System.out.println("============================");
+                    break;
+
+
+                case 2:
+                    segundo = "";
+                    System.out.println("Contato 2 foi apagado");
+                    System.out.println("============================");
+                    break;
+
+                case 3:
+                    terceiro = "";
+                    System.out.println("Contato 3 foi apagado");
+                    System.out.println("============================");
+                    break;
+
+                default:
+                    System.out.println("Número inválido! Aceitamos apenas: 1,2,3");
+            }
+
+            // VERIFICANDO SE O CLIENTE DESEJA EXIBIR A LISTA DE CONTATOS
+            System.out.println("Deseja exibir a lista novamente[S/N]: ");
+            String escolha = novoSc.nextLine();
+
+            if (escolha.equalsIgnoreCase("S")) {
+                listaDeContatos(primeiro, segundo, terceiro);
+                System.out.println("LISTA ENCERRADA");
+
+            } else {
                 System.out.println("============================");
-                break;
-
-
-            case 2:
-                segundo = "";
-                System.out.println("Contato 2 foi apagado");
-                System.out.println("============================");
-                break;
-
-            case 3:
-                terceiro = "";
-                System.out.println("Contato 3 foi apagado");
-                System.out.println("============================");
-                break;
-
-            default:
-                System.out.println("Número inválido! Aceitamos apenas: 1,2,3");
-        }
-
-        // VERIFICANDO SE O CLIENTE DESEJA EXIBIR A LISTA DE CONTATOS
-        System.out.println("Deseja exibir a lista novamente[S/N]: ");
-        String escolha = novoSc.next();
-
-        if(escolha.equalsIgnoreCase("S")){
-            listaDeContatos(primeiro,segundo, terceiro);
+                System.out.println("FIM DA LISTA DE CONTATOS");
+            }
         } else {
-            System.out.println("============================");
-            System.out.println("FIM DA LISTA DE CONTATOS");
+            System.out.println("LISTA ENCERRADA");
         }
-
 
         }
 }
